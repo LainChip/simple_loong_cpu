9a950beb2b4bb647a04c44e87829a3e0aece3c78
Dofingert <dofingert@gmail.com>
2023-01-06 13:25:05 +0800

    crlf

--- rtl/axi/axi_converter.sv
+++ rtl/axi/axi_converter.sv
@@ -7,7 +7,7 @@ module axi_converter#(
     parameter int CACHE_PORT_NUM = 2
 )(
     input clk,input rst_n,
-	AXI_BUS.Master   axi_bus, // 来自pulp_axi 库
+	AXI_BUS.Master   axi_bus_if, // 来自pulp_axi 库
     input cache_bus_req_t  [CACHE_PORT_NUM - 1 : 0]req_i,       // cache的访问请求
     output cache_bus_resp_t [CACHE_PORT_NUM - 1 : 0]resp_o       // cache的访问应答
 );
