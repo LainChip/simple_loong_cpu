72d37a63cfeac135d10dbbe7cce5e75deeb75217
Dofingert <dofingert@gmail.com>
2023-01-09 22:01:27 +0800

    为lru添加testbench

--- rtl/lsu/lsu.sv
+++ rtl/lsu/lsu.sv
@@ -1,4 +1,5 @@
 `include "common.svh"
+`include "decoder.svh"
 `include "lsu_types.svh"
 
 `ifdef __DLSU_VER_1
@@ -8,7 +9,7 @@ module lsu (
 	input rst_n,  // Asynchronous reset active low
 	
 	// 控制信号
-	input decode_info_t[1:0] decode_info_i,
+	input decode_info_t [1:0] decode_info_i,
 	
 	// 流水线数据输入输出
 	input logic[1:0][31:0] vaddr_i,
@@ -18,11 +19,11 @@ module lsu (
 
 	// 连接内存总线
 	output cache_bus_req_t bus_req_o,
-	input cache_bus_req_t bus_resp_i,
+	input cache_bus_resp_t bus_resp_i,
 
 	// 握手信号
-	output busy,
-	input stall
+	output busy_o,
+	input stall_i
 );
 
 	typedef struct packed{
@@ -34,11 +35,16 @@ module lsu (
 	} inner_mem_req_t;
 
 	inner_mem_req_t mem_req_comb,mem_req_stage_1,mem_req_stage_2;
+	logic[31:0] r_data;
 	logic[2:0] fsm_state,fsm_state_next;
 	logic transfer_done;
 	localparam STATE_IDLE = 3'b001;
 	localparam STATE_ADDR = 3'b010;
 	localparam STATE_DATA = 3'b100;
+
+	// 输出数据
+	assign r_data_o[0] = r_data;
+	assign r_data_o[1] = r_data;
 
 	// 获取需要的控制信息，进行流水
 	always_comb begin
@@ -53,20 +59,20 @@ module lsu (
 		end
 		mem_req_comb.mem_sel_1 = decode_info_i[1].m1.mem_valid;
 	end
-	always_ff begin
-		if(clk) begin
+	always_ff@(posedge clk) begin
+		if(rst_n) begin
 			mem_req_stage_1 <= '0;
 			mem_req_stage_2 <= '0;
-		end else if(~busy & ~stall) begin
+		end else if(~busy_o & ~stall_i) begin
 			mem_req_stage_1 <= mem_req_comb;
 			{mem_req_stage_2.mem_type,mem_req_stage_2.mem_write,mem_req_stage_2.mem_sel_1} <= 
 			{mem_req_stage_1.mem_type,mem_req_stage_1.mem_write,mem_req_stage_1.mem_sel_1};
+			mem_req_stage_2.mem_addr <= paddr_i[mem_req_stage_1.mem_sel_1];
 			if(transfer_done) begin
 				mem_req_stage_2.mem_valid <= '0;
 			end else begin
 				mem_req_stage_2.mem_valid <= mem_req_stage_1.mem_valid;
 			end
-			mem_req_stage_2.mem_addr <= paddr_i[mem_req_stage_1.mem_sel_1];
 		end
 	end
 
@@ -74,7 +80,6 @@ module lsu (
 	assign transfer_done = ((mem_req_stage_2.mem_write & bus_req_o.data_last) | 
 				(~mem_req_stage_2.mem_write & bus_resp_i.data_last)) & bus_resp_i.data_ok & bus_req_o.data_ok;
 	always_comb begin
-		fsm_state_next = fsm_state;
 		case(fsm_state)
 			STATE_IDLE:begin
 				if(mem_req_stage_2.mem_valid) begin
@@ -91,7 +96,18 @@ module lsu (
 					fsm_state_next = STATE_IDLE;
 				end
 			end
+			default:begin
+				fsm_state_next = fsm_state;
+			end
 		endcase
+	end
+	assign busy_o = (fsm_state != STATE_IDLE) | mem_req_stage_2.mem_valid;
+	always_ff @(posedge clk) begin
+		if(~rst_n) begin
+			fsm_state <= STATE_IDLE;
+		end else begin
+			fsm_state <= fsm_state_next;
+		end
 	end
 
 	// 总线请求赋值
@@ -104,23 +120,30 @@ module lsu (
 
 		bus_req_o.w_data = w_data_i[mem_req_stage_2.mem_sel_1] << mem_req_stage_2.mem_addr[1:0];
 		case(mem_req_stage_2.mem_type)
-			`MEM_TYPE_WORD: begin
+			`_MEM_TYPE_WORD: begin
 				bus_req_o.data_strobe = 4'b1111;
 			end
-			`MEM_TYPE_HALF: begin
+			`_MEM_TYPE_HALF: begin
 				bus_req_o.data_strobe = (4'b0011 << mem_req_stage_2.mem_addr[1]);
 			end
-			`MEM_TYPE_BYTE: begin
+			`_MEM_TYPE_BYTE: begin
 				bus_req_o.data_strobe = (4'b0001 << mem_req_stage_2.mem_addr[1:0]);
+			end
+			default: begin
+				bus_req_o.data_strobe = 4'b0000;
 			end
 		endcase
 		bus_req_o.data_ok = fsm_state == STATE_DATA;
 		bus_req_o.data_last = fsm_state == STATE_DATA;
 	end
 
-	// 返回请求处理
-	
+	// 输出数据处理
+	always_ff @(posedge clk) begin
+		if(transfer_done) begin
+			r_data <= bus_resp_i.r_data;
+		end
+	end
 
-endmodule : lsu
+endmodule
 
 `endif
---
+++ rtl/lsu/lsu_test_plantform.sv
@@ -0,0 +1,9 @@
+`include "common.svh"
+`include "lsu_types.svh"
+
+module lsu_test_plantform (
+	input clk,    // Clock
+	input rst_n  // Asynchronous reset active low
+;
+
+endmodule : lsu_test_plantform
