cf9a2e4ac59b70c96a46a88d20506b696c9e7f6c
suyang <2506806016@qq.com>
2023-01-13 10:22:24 +0800

    更改bpf接口

--- rtl/bpu/bpf.sv
+++ rtl/bpu/bpf.sv
@@ -16,18 +16,19 @@ module bpf (
 	input clk,    // Clock
 	input rst_n,  // Asynchronous reset active low
 	input [31:0] pc_i,
-	input [25:0] offs_i,
 	input [31:0] rj_i,
 	input [31:0] rd_i,
-	input [4:0] rj_index_i,
-	input [4:0] rd_index_i,
-	input reg_type_t reg_type_i,
-	input cmp_type_t cmp_type_i,
+	input decode_info_t decode_i,
 	input branch_type_t br_type_i,
 	input bpu_predict_t predict_i,
 	output bpu_update_t update_o,
 	output [31:0] target_o
 );
+
+	wire [25:0] offs_i = decode_i.general.inst25_0; 
+	wire [4:0] rj_index_i = decode_i.general.inst25_0[9:5];
+	wire [4:0] rd_index_i = decode_i.general.inst25_0[4:0];
+	cmp_type_t cmp_type_i = decode_i.ex.cmp_type;
 	
 	wire [31:0] offs_26 = {{6{offs_i[25]}}, offs_i};
 	wire [31:0] offs_16 = {{16{offs_i[25]}}, offs_i[25:10]};
@@ -66,7 +67,7 @@ module bpf (
 
 	assign update_o.btb_update = update_o.flush;
 	always_comb begin : proc_br_type
-		if ((br_type_i == `_BRANCH_INDIRECT && rd_index_i == 1) || reg_type_i == `_REG_TYPE_BL) begin
+		if ((br_type_i == `_BRANCH_INDIRECT && rd_index_i == 1) || decode_i.ex.branch_link) begin
 			update_o.br_type = `_CALL;
 		end else if (br_type_i == `_BRANCH_INDIRECT && rj_index_i == 1 && offs_16 == 0) begin
 			update_o.br_type = `_RETURN;
--- rtl/decoder/decoder.svh
+++ rtl/decoder/decoder.svh
@@ -1,38 +1,6 @@
 `ifndef _DECODE_HEADER
 `define _DECODE_HEADER
 
-`define _ALU_TYPE_NIL (4'd0)
-`define _ALU_TYPE_ADD (4'd1)
-`define _ALU_TYPE_SUB (4'd2)
-`define _ALU_TYPE_SLT (4'd3)
-`define _ALU_TYPE_AND (4'd4)
-`define _ALU_TYPE_OR (4'd5)
-`define _ALU_TYPE_XOR (4'd6)
-`define _ALU_TYPE_NOR (4'd7)
-`define _ALU_TYPE_SL (4'd8)
-`define _ALU_TYPE_SR (4'd9)
-`define _ALU_TYPE_MUL (4'd10)
-`define _ALU_TYPE_MULH (4'd11)
-`define _ALU_TYPE_DIV (4'd12)
-`define _ALU_TYPE_MOD (4'd13)
-`define _ALU_TYPE_LUI (4'd14)
-`define _OPD_REG (3'b000)
-`define _OPD_IMM_U5 (3'b001)
-`define _OPD_IMM_S12 (3'b010)
-`define _OPD_IMM_U12 (3'b011)
-`define _OPD_IMM_S20 (3'b100)
-`define _BRANCH_INVALID (2'b00)
-`define _BRANCH_IMMEDIATE (2'b01)
-`define _BRANCH_INDIRECT (2'b10)
-`define _BRANCH_CONDITION (2'b11)
-`define _CMP_EQL (3'd0)
-`define _CMP_NEQ (3'd1)
-`define _CMP_LSS (3'd2)
-`define _CMP_GER (3'd3)
-`define _CMP_LEQ (3'd4)
-`define _CMP_GEQ (3'd5)
-`define _CMP_LTU (3'd6)
-`define _CMP_GEU (3'd7)
 `define _CSR_CRMD (14'h0)
 `define _CSR_PRMD (14'h1)
 `define _CSR_ECTL (14'h4)
@@ -82,6 +50,18 @@
 `define _READY_M2 (1'b1)
 `define _USE_EX (1'b0)
 `define _USE_M2 (1'b1)
+`define _BRANCH_INVALID (2'b00)
+`define _BRANCH_IMMEDIATE (2'b01)
+`define _BRANCH_INDIRECT (2'b10)
+`define _BRANCH_CONDITION (2'b11)
+`define _CMP_EQL (3'd0)
+`define _CMP_NEQ (3'd1)
+`define _CMP_LSS (3'd2)
+`define _CMP_GER (3'd3)
+`define _CMP_LEQ (3'd4)
+`define _CMP_GEQ (3'd5)
+`define _CMP_LTU (3'd6)
+`define _CMP_GEU (3'd7)
 `define _MEM_TYPE_NONE (3'd0)
 `define _MEM_TYPE_WORD (3'd1)
 `define _MEM_TYPE_HALF (3'd2)
@@ -89,13 +69,28 @@
 `define _MEM_TYPE_UWORD (3'd5)
 `define _MEM_TYPE_UHALF (3'd6)
 `define _MEM_TYPE_UBYTE (3'd7)
+`define _ALU_TYPE_NIL (4'd0)
+`define _ALU_TYPE_ADD (4'd1)
+`define _ALU_TYPE_SUB (4'd2)
+`define _ALU_TYPE_SLT (4'd3)
+`define _ALU_TYPE_AND (4'd4)
+`define _ALU_TYPE_OR (4'd5)
+`define _ALU_TYPE_XOR (4'd6)
+`define _ALU_TYPE_NOR (4'd7)
+`define _ALU_TYPE_SL (4'd8)
+`define _ALU_TYPE_SR (4'd9)
+`define _ALU_TYPE_MUL (4'd10)
+`define _ALU_TYPE_MULH (4'd11)
+`define _ALU_TYPE_DIV (4'd12)
+`define _ALU_TYPE_MOD (4'd13)
+`define _ALU_TYPE_LUI (4'd14)
+`define _OPD_REG (3'b000)
+`define _OPD_IMM_U5 (3'b001)
+`define _OPD_IMM_S12 (3'b010)
+`define _OPD_IMM_U12 (3'b011)
+`define _OPD_IMM_S20 (3'b100)
 
 typedef logic[25 : 0] inst25_0_t;
-typedef logic[3 : 0] alu_type_t;
-typedef logic[2 : 0] opd_type_t;
-typedef logic[0 : 0] opd_unsigned_t;
-typedef logic[1 : 0] branch_type_t;
-typedef logic[2 : 0] cmp_type_t;
 typedef logic[1 : 0] rdcntv_type_t;
 typedef logic[0 : 0] do_rdcntid_t;
 typedef logic[0 : 0] do_csrrd_t;
@@ -109,9 +104,15 @@ typedef logic[0 : 0] invtlb_en_t;
 typedef logic[0 : 0] ready_time_t;
 typedef logic[1 : 0] use_time_t;
 typedef logic[2 : 0] reg_type_t;
+typedef logic[1 : 0] branch_type_t;
+typedef logic[2 : 0] cmp_type_t;
+typedef logic[0 : 0] branch_link_t;
 typedef logic[2 : 0] mem_type_t;
 typedef logic[0 : 0] mem_write_t;
 typedef logic[0 : 0] mem_valid_t;
+typedef logic[3 : 0] alu_type_t;
+typedef logic[2 : 0] opd_type_t;
+typedef logic[0 : 0] opd_unsigned_t;
 
 typedef struct packed {
     tlbwr_en_t tlbwr_en;
@@ -123,20 +124,10 @@ typedef struct packed {
 }m1_t;
 
 typedef struct packed {
-    inst25_0_t inst25_0;
-}general_t;
-
-typedef struct packed {
     ready_time_t ready_time;
     use_time_t use_time;
     reg_type_t reg_type;
 }is_t;
-
-typedef struct packed {
-    rdcntv_type_t rdcntv_type;
-    do_rdcntid_t do_rdcntid;
-    do_csrrd_t do_csrrd;
-}wb_t;
 
 typedef struct packed {
     csr_num_t csr_num;
@@ -146,19 +137,30 @@ typedef struct packed {
 }m2_t;
 
 typedef struct packed {
+    rdcntv_type_t rdcntv_type;
+    do_rdcntid_t do_rdcntid;
+    do_csrrd_t do_csrrd;
+}wb_t;
+
+typedef struct packed {
+    inst25_0_t inst25_0;
+}general_t;
+
+typedef struct packed {
+    branch_type_t branch_type;
+    cmp_type_t cmp_type;
+    branch_link_t branch_link;
     alu_type_t alu_type;
     opd_type_t opd_type;
     opd_unsigned_t opd_unsigned;
-    branch_type_t branch_type;
-    cmp_type_t cmp_type;
 }ex_t;
 
 typedef struct packed {
     m1_t m1;
-    general_t general;
     is_t is;
-    wb_t wb;
     m2_t m2;
+    wb_t wb;
+    general_t general;
     ex_t ex;
 }decode_info_t;
 
