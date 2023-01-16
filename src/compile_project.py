bacb31d1d62ab8d3ac13b42fa4e30ceda496fa9e
Dofingert <dofingert@gmail.com>
2023-01-16 22:53:55 +0800

    添加编译项目脚本

--- src/compile_project.py
+++ src/compile_project.py
@@ -1,3 +1,15 @@
 import os
+# try_chiplab_home = os.getenv('CHIPLAB_HOME')
+sv_file_list = ['./inst/decoder.sv','./inst/decoder.svh']
+os.system("cd inst/ && python gen_decoder.py")
+for root, dirs, files in os.walk('../rtl'):
+    for file in files:
+        path = os.path.join(root, file)
+        ext_name = os.path.splitext(path)[1]
+        if (ext_name == '.sv' or ext_name == '.svh' or ext_name == '.v' or ext_name == '.vh') and 'logs/annotated/' not in path and 'decoder.sv' not in path and 'decoder.svh' not in path:
+            sv_file_list.append(path)
 
-try_chiplab_home = os.getenv('CHIPLAB_HOME')
+os.system("rm -r ../dist && mkdir ../dist")
+for file_path in sv_file_list:
+    print(file_path)
+    os.system("cp " + file_path + " ../dist/")
