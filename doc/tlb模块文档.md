7852f90213c0c40440596b123c0711fc51e874bf
Arthuring <20373091@buaa.edu.cn>
2023-01-20 02:17:52 +0800

    添加tlb接口文档

--- doc/tlb模块文档.md
+++ doc/tlb模块文档.md
@@ -2,4 +2,26 @@
 
 ## 模块定义
 
+```systemverilog
+module tlb #(
+    parameter int TLB_ENTRY_NUM = `TLB_ENTRY_NUM,
+    parameter int TLB_PORT = 2
+)
+(
+    input                                       clk         ,
+    input                                       rst_n       ,
+    //search
+    input   tlb_s_req_t     [TLB_PORT - 1 : 0]  s_req_i     ,
+    output  tlb_s_resp_t    [TLB_PORT - 1 : 0]  s_resp_o    ,
+    //write
+    input   tlb_w_req_t                         w_req_i     ,
+    //read
+    input   [$clog2(`TLB_ENTRY_NUM)-1:0]        r_index_i   ,
+    output  tlb_r_resp_t                        r_resp_o    ,
+    //invalid
+    input   tlb_inv_req_t                       inv_req_i   
+
+);
+```
+
 ## 模块时序说明
--- rtl/tlb/tlb.svh
+++ rtl/tlb/tlb.svh
@@ -1,4 +1,70 @@
 `ifndef _TLB_HEADER
 `define _TLB_HEADER
 
+`define _TLB_ENTRY_NUM 32
+
+typedef struct packed {  
+    logic                                    fetch     ;
+    logic  [18:0]                            vppn      ;
+    logic                                    odd_page  ;
+    logic  [ 9:0]                            asid      ;
+} tlb_s_req_t;
+
+typedef struct packed {
+    logic                                    found     ;
+    logic [$clog2(`_TLB_ENTRY_NUM)-1:0]      index     ;
+    logic [ 5:0]                             ps        ;
+    logic [19:0]                             ppn       ;
+    logic                                    v         ;
+    logic                                    d         ;
+    logic [ 1:0]                             mat       ;
+    logic [ 1:0]                             plv       ;
+    
+} tlb_s_resp_t;
+
+typedef struct packed {
+    logic                                    we        ;
+    logic  [$clog2(TLBNUM)-1:0]              index     ;
+    logic  [18:0]                            vppn      ;
+    logic  [ 9:0]                            asid      ;
+    logic                                    g         ;
+    logic  [ 5:0]                            ps        ;
+    logic                                    e         ;
+    logic                                    v0        ;
+    logic                                    d0        ;
+    logic  [ 1:0]                            mat0      ;
+    logic  [ 1:0]                            plv0      ;
+    logic  [19:0]                            ppn0      ;
+    logic                                    v1        ;
+    logic                                    d1        ;
+    logic  [ 1:0]                            mat1      ;
+    logic  [ 1:0]                            plv1      ;
+    logic  [19:0]                            ppn1      ;
+    
+} tlb_w_req_t;
+
+typedef struct packed {
+    logic [18:0]                             vppn      ;
+    logic [ 9:0]                             asid      ;
+    logic                                    g         ;
+    logic [ 5:0]                             ps        ;
+    logic                                    e         ;
+    logic                                    v0        ;
+    logic                                    d0        ;
+    logic [ 1:0]                             mat0      ;
+    logic [ 1:0]                             plv0      ;
+    logic [19:0]                             ppn0      ;
+    logic                                    v1        ;
+    logic                                    d1        ;
+    logic [ 1:0]                             mat1      ;
+    logic [ 1:0]                             plv1      ;
+    logic [19:0]                             ppn1      ;
+} tlb_r_resp_t;
+
+typedef struct packed {
+    logic                                    en        ;
+    logic  [ 4:0]                            op        ;
+    logic  [ 9:0]                            asid      ;
+    logic  [18:0]                            vpn       ;
+} tlb_inv_req_t;
 `endif
