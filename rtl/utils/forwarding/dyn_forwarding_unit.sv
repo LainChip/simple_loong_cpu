55e05e2c4996882929237a09b7a271c1841ad707
Elucidator-xrb <rubixue572@gmail.com>
2023-07-07 14:56:49 +0800

    clear v2, set it to unique

--- rtl/utils/forwarding/dyn_forwarding_unit.sv
+++ rtl/utils/forwarding/dyn_forwarding_unit.sv
@@ -1,4 +1,4 @@
-`include "common.svh"
+`include "../common.svh"
 `include "forwarding_type.svh"
 
 module dyn_forwarding_unit #(
@@ -11,21 +11,21 @@ module dyn_forwarding_unit #(
 );
     /* assume forward_bus_i: high bit with high priority */
 
-    forwarding_data_t [SRC_NUM-1:0] forwarding_src;
+    forwarding_data_t [SRC_NUM-1 : 0] forwarding_src;
     logic [SRC_NUM-1 : 0] cmp_res;     
-    assign forwarding_src = forward_bus_i; // for potential extention
+    assign forwarding_src = forwarding_bus_i; // for potential extention
     generate
         for (genvar i = 0; i < SRC_NUM; i++) begin
             assign cmp_res[i] = (data_raw_i.addr == forwarding_src[i].addr);
         end
     endgenerate
 
-    logic [$clog(SRC_NUM)-1:0] leading_zero;
+    logic [$clog2(SRC_NUM)-1 : 0] leading_zero;
     logic sel_raw;
-    lzc count_leading_zero #(
+    lzc #(
         .WIDTH(SRC_NUM),
-        .mode (1)
-    )(
+        .MODE (1'b1)
+    ) count_leading_zero(
         .in_i(cmp_res),
         .cnt_o(leading_zero),
         .empty_o(sel_raw)
@@ -35,7 +35,8 @@ module dyn_forwarding_unit #(
         if (sel_raw) begin
             data_forwarding_o = data_raw_i;
         end else begin
-            data_forwarding_o = data_src[SRC_NUM-1-leading_zero]; // can '-' be improved ?
+            data_forwarding_o = forwarding_src[SRC_NUM-1-leading_zero]; 
+            // can '-' be improved ?
         end
     end
 
