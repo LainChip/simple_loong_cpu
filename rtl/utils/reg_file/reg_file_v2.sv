c00a4c2bd070d88308f4c93523388fb6f4f18fca
Elucidator-xrb <rubixue572@gmail.com>
2023-07-08 16:39:38 +0800

    add wen signal & remove mux of read_data

--- rtl/utils/reg_file/reg_file_v2.sv
+++ rtl/utils/reg_file/reg_file_v2.sv
@@ -16,18 +16,11 @@ module reg_file #(
     output  reg_data_t [3:0] r_data_o,
     // write port
     input   reg_addr_t [1:0] w_addr_i,
-    output  reg_data_t [1:0] w_data_i
+    input   reg_data_t [1:0] w_data_i,
+    input   logic [1:0] w_en_i
 );
     localparam READ_PORT  = 4;
     localparam WRITE_PORT = 2;
-
-    reg_data_t [3:0] read_data;
-
-    generate
-        for (genvar i = 0; i < READ_PORT; i++) begin
-            la_mux2 #(DATA_WIDTH)read_out_i('0, read_data[i], r_data_o[i], (r_addr_i[i] == '0));
-        end
-    endgenerate
 
     bank_mpregfiles_4r2w #(
         .WIDTH(32),
@@ -40,17 +33,17 @@ module reg_file #(
         .ra1_i(r_addr_i[1]),
         .ra2_i(r_addr_i[2]),
         .ra3_i(r_addr_i[3]),
-        .rd0_o(read_data[0]),
-        .rd1_o(read_data[1]),
-        .rd2_o(read_data[2]),
-        .rd3_o(read_data[3]),
+        .rd0_o(r_data_o[0]),
+        .rd1_o(r_data_o[1]),
+        .rd2_o(r_data_o[2]),
+        .rd3_o(r_data_o[3]),
         // write port
         .wd0_i(w_data_i[0]),
         .wd1_i(w_data_i[1]),
         .wa0_i(w_addr_i[0]),
         .wa1_i(w_addr_i[1]),
-        .we0_i(~(w_addr_i[0] == '0)),
-        .we1_i(~(w_addr_i[0] == '0)),
+        .we0_i(w_en_i[0] & ~(w_addr_i[0] == '0)),
+        .we1_i(w_en_i[1] & ~(w_addr_i[0] == '0)),
         // signal
         .conflict_o()
     );
