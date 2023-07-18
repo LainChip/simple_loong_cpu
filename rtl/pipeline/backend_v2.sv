596634d1baf2355eafab756845449d7d39101f9b
buaa-dofingert <dofingert@gmail.com>
2023-07-18 17:12:50 +0800

    完成流水线主体框架

---
+++ rtl/pipeline/backend_v2.sv
@@ -0,0 +1,78 @@
+`include "pipeline.svh"
+`include "../lsu/lsu_types.svh"
+
+module backend(
+    input logic clk,
+    input logic rst_n,
+
+    input frontend_req_t frontend_req_i,
+    output frontend_resp_t frontend_resp_o,
+
+    input cache_bus_resp_t bus_resp_i,
+    output cache_bus_req_t bus_req_o
+  );
+  /* ------ ------ ------ ------ ------ IS 级 ------ ------ ------ ------ ------ */
+  // ISSUE 级别：
+  // 判定来自前段的指令能否发射
+
+  // 读取寄存器堆，或者生成立即数
+
+  // 读取 scoreboard，判断寄存器值是否有效
+
+  // 产生 EX 级的流水线信号 x 2
+  // IS 数据前递部分（EX、WB） 不完全。
+
+  logic ex_skid_ready_q,ex_skid_valid;
+  logic ex_ready, ex_valid;
+  /* SKID BUF(可选的) */
+  // SKID 数据前递部分（M2、WB） 不完全。
+  // SKID BUF 对于 scoreboard 来说应该是透明的，使用 valid-ready 握手
+  /* SKID BUF 结束*/
+  /* ------ ------ ------ ------ ------ EX 级 ------ ------ ------ ------ ------ */
+
+  logic ex_stall_req, ex_stall;
+  logic m1_stall_req, m1_stall;
+  logic m2_stall_req, m2_stall;
+  logic wb_stall_req, wb_stall;
+
+  // STALL MANAGER
+  // 后续级可以阻塞前级就算有气泡，也不可以前进。但前级不能阻塞后级。
+  // EX 级别
+
+  // EX 数据前递部分（M1、M2、WB） 完全，需要验证有效性。
+  // 输入来自 IS-EX 级别的流水线寄存器。
+  // 当 EX 级没有暂停时，EX 的流水线寄存器 来自 IS / SKID BUF
+  // 当 EX 级暂停的时候（ex_stall == 1），EX 的流水线寄存器，来自转发后的 EX
+
+  // EX 的 FU 部分，接入 ALU、乘法器、除法队列 pusher（Optional）
+
+  // EX 的额外部分
+  // EX 级别的访存地址计算 / 地址翻译逻辑
+
+  // EX 级别的目标地址计算
+  // 接入 dcache
+
+  /* ------ ------ ------ ------ ------ M1 级 ------ ------ ------ ------ ------ */
+  // M1 数据接受前递部分（M2、WB） 完全
+
+  // M1 的 FU 部分，接入 ALU、LSU（EARLY）
+
+  // M1 的额外部分
+  // CSR读写地址的输入 / 跳转及异常的处理 ， BARRIER 指令的执行（DBAR、 IBAR）。
+
+  /* ------ ------ ------ ------ ------ M2 级 ------ ------ ------ ------ ------ */
+  // M2 数据接受前递部分（WB）完全
+
+  // M2 的 FU 部分，接入 ALU、LSU、MUL、CSR
+
+  // M2 的额外部分
+  // CSR 修改相关指令的执行，如写 CSR、写 TLB、缓存控制均在此处执行。
+
+  /* ------ ------ ------ ------ ------ WB 级 ------ ------ ------ ------ ------ */
+  // 不存在数据前递
+
+  // WB 的 FU 部分，接入 DIV，等待 DIV 完成。
+
+  // WB 需要接回 IS 级的 寄存器堆和 scoreboard
+
+endmodule
--- rtl/pipeline/pipeline.svh
+++ rtl/pipeline/pipeline.svh
@@ -3,95 +3,107 @@
 
 `include "decoder.svh"
 
-// TODO
-typedef logic priv_resp_t;
+        // TODO
+        typedef logic priv_resp_t;
 typedef logic priv_req_t;
 
 // 解码出来的寄存器信息
 typedef struct packed{
-	logic [1:0][4:0] r_reg; // 0 for rk, 1 for rj
-	logic [4:0] w_reg;
-} reg_info_t;
+          logic [1:0][4:0] r_reg; // 0 for rk, 1 for rj
+          logic [4:0] w_reg;
+        } reg_info_t;
 
 // 发射后的寄存器信息
 typedef struct packed{
-	logic [1:0][4:0] r_addr;
-	logic [1:0][3:0] r_id;
-	logic [1:0] r_ready;
-} read_flow_t;
+          logic [1:0][4:0] r_addr;
+          logic [1:0][3:0] r_id;
+          logic [1:0] r_ready;
+        } read_flow_t;
 typedef struct packed{
-	logic [4:0] w_addr;
-	logic [2:0] w_id;
-	logic w_valid;
-} write_flow_t;
+          logic [4:0] w_addr;
+          logic [2:0] w_id;
+          logic w_valid;
+        } write_flow_t;
 // 控制流，目前未进行精简。
 typedef struct packed {
-	logic valid_inst;  // 标记指令是否有效（包含推测执行 / 确定执行） ::: 需要被 rst clr
-	logic need_commit; // 标记指令是否可提交，在 M2 级才是确定值     ::: 需要被 rst clr && 被跳转信号 clr
-} exc_flow_t;
+          logic valid_inst;  // 标记指令是否有效（包含推测执行 / 确定执行） ::: 需要被 rst clr
+          logic need_commit; // 标记指令是否可提交，在 M2 级才是确定值     ::: 需要被 rst clr && 被跳转信号 clr
+        } exc_flow_t;
 
 // 异常流
 typedef struct packed {
-	logic adef;
-	logic tlbr;
-	logic pif;
-	logic ppi;
-} fetch_excp_t;
+          logic adef;
+          logic tlbr;
+          logic pif;
+          logic ppi;
+        } fetch_excp_t;
 typedef struct packed {
-	logic adem;
-	logic ale;
+          logic adem;
+          logic ale;
 
-	// FRONTEND
-	logic adef;
-	logic itlbr;
-	logic pif;
-	logic ippi;
-} excp_flow_t;
+          // FRONTEND
+          logic adef;
+          logic itlbr;
+          logic pif;
+          logic ippi;
+        } excp_flow_t;
 
 // 输入到后端的指令流
 typedef struct packed {
-	is_t decode_info;
-	reg_info_t reg_info;
-	bpu_into_t bpu_predict;
-	fetch_excp_t fetch_excp;
-	logic[31:0] pc;
-} inst_t;
+          is_t decode_info;
+          reg_info_t reg_info;
+          bpu_into_t bpu_predict;
+          fetch_excp_t fetch_excp;
+          logic[31:0] pc;
+        } inst_t;
 
 typedef struct packed{
-	ex_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
-	bpu_predict_t bpu_predict;
-	fetch_excp_t fetch_excp;
-	logic[31:0] pc;
-} pipeline_ctrl_ex_t; // 移位寄存器实现的部分
+          ex_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
+          bpu_predict_t bpu_predict;
+          fetch_excp_t fetch_excp;
+		  logic[25:0] addr_imm;
+          logic[31:0] pc;
+        } pipeline_ctrl_ex_t; // 移位寄存器实现的部分
 
 typedef struct packed{
-	m1_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
-	bpu_predict_t bpu_predict;
-	excp_flow_t excp_flow;
-	logic[31:0] pc;
-} pipeline_ctrl_m1_t; // 移位寄存器实现的部分
+          m1_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
+          bpu_predict_t bpu_predict;
+          excp_flow_t excp_flow;
+		  logic[13:0] csr_id;
+		  logic[31:0] jump_target;
+		  logic[31:0] vaddr; 
+		  logic[31:0] pc;
+        } pipeline_ctrl_m1_t; // 移位寄存器实现的部分
 
 typedef struct packed{
-	m2_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
-	logic[31:0] pc;
-} pipeline_ctrl_m2_t; // 移位寄存器实现的部分
+          m2_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
+		  logic[31:0] vaddr;
+		  logic[31:0] paddr; 
+		  logic[31:0] pc;
+        } pipeline_ctrl_m2_t; // 移位寄存器实现的部分
 
 typedef struct packed{
-	wb_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
-	logic[31:0] pc;
-} pipeline_ctrl_wb_t; // 移位寄存器实现的部分
+          wb_t decode_info;  // 指令控制信息 ::: 不需要 rst clr | 跳转 clr
+          logic[31:0] pc;
+        } pipeline_ctrl_wb_t; // 移位寄存器实现的部分
 
 typedef struct packed{
-	read_flow_t r_flow;
-	logic[1:0][31:0] r_data;
-	write_flow_t w_flow;
-	logic[31:0] w_data;
-} pipeline_data_t; // 无法使用移位寄存器实现，普通寄存器
+          read_flow_t r_flow;
+          logic[1:0][31:0] r_data;
+          write_flow_t w_flow;
+          logic[31:0] w_data;
+        } pipeline_data_t; // 无法使用移位寄存器实现，普通寄存器
 
 typedef struct packed {
-    logic [31:0] data;  // reg data
-    logic [2 :0] id;    // reg addr
-    logic valid;        // whether data is valid
-} fwd_data_t;
+          logic [31:0] data;  // reg data
+          logic [2 :0] id;    // reg addr
+          logic valid;        // whether data is valid
+        } fwd_data_t;
+typedef struct packed {
+          logic empty;
+        }frontend_req_t;
+typedef struct packed {
+          logic empty;
+        }frontend_resp_t;
 
 `endif
