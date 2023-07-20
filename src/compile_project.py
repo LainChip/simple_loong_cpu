2c38e6654c510590e0f211bb8c35f54157011ce7
buaa-dofingert <dofingert@gmail.com>
2023-07-20 21:00:47 +0800

    添加构建flag,避免早期 cpu相关文件被错误加入

--- src/compile_project.py
+++ src/compile_project.py
@@ -19,9 +19,7 @@ for root, dirs, files in os.walk('../rtl'):
     for file_name in files:
         path = os.path.join(root, file_name)
         ext_name = os.path.splitext(path)[1]
-        import_flag = True
-        if (ext_name == '.sv' or ext_name == '.svh' or ext_name == '.v' or ext_name == '.vh') and 'logs/annotated/' not in path and 'decoder.sv' not in path and 'decoder.svh' not in path:
-            import_flag = True
+        if (ext_name == '.sv' or ext_name == '.svh' or ext_name == '.v' or ext_name == '.vh') and 'deperated' not in path and 'logs/annotated/' not in path and 'decoder.sv' not in path and 'decoder.svh' not in path:
             with open(path) as f:
                 f = f.read()
                 f = re.findall(r'--JSON--(.*)--JSON--',f)
